97122b06871f5fac448e8cbc35c2e17771a13901
Kevin De Marchi <kevindemarchipro@gmail.com>
2025-12-12 09:59:00 +0100

    fix

--- src/components/Autocomplete.component.tsx
+++ src/components/Autocomplete.component.tsx
@@ -134,6 +134,7 @@ const Autocomplete: FC<IProps> = ({
             >
               <LiquidGlass
                 borderRadius={20}
+                backgroundColor="rgba(255, 255, 255, 0.5)"
                 className="flex flex-col gap-5 justify-center items-center w-full py-2"
               >
                 <div className="flex flex-col gap-2 max-h-60 overflow-y-scroll">
